9a5732713dc33ca7d381c1e74ecbf4b59c74624d
The Phabulous Phantom <thephabulousphantom@gmail.com>
2023-12-30 14:21:48 +0100

    sw 17

--- foota/serviceWorker.js
+++ foota/serviceWorker.js
@@ -1,7 +1,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const gameVersion = "v1.16";
+const gameVersion = "v1.17";
 const PRECACHE = "astroPirouette-precache-" + gameVersion;
 const LAZYLOAD = 'astroPirouette-lazyload-' + gameVersion;
 const RUNTIME = 'astroPirouette-runtime';
