e57c8851e6fe516821b121b20152731b00ba548c
The Phabulous Phantom <thephabulousphantom@gmail.com>
2023-12-30 05:01:24 +0100

    adding rotation to body instead of html tag

--- foota/js/screenConfig.js
+++ foota/js/screenConfig.js
@@ -142,7 +142,7 @@ export default class screenConfig extends Screen {
         World.camera.position.x = this.rotationRadius * Math.sin(time / 2000);
         World.camera.position.y = this.rotationRadius * Math.cos(time / 2000);
         
-        document.documentElement.style.setProperty('--backgroundAngle', `${this.directionCurrent / 0.0174532925}deg`);
+        document.getElementByTagName("body").style.setProperty('--backgroundAngle', `${this.directionCurrent / 0.0174532925}deg`);
 
         //World.camera.position.y = 0;
     }
