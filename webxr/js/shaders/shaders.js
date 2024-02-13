279f539004690a1567340a5e8e2dbb381b34aa14
The Phabulous Phantom <thephabulousphantom@gmail.com>
2024-02-13 22:49:47 +0100

    simplex3d

--- webxr/js/shaders/shaders.js
+++ webxr/js/shaders/shaders.js
@@ -322,40 +322,106 @@ export const NoiseShader = new Shader(
         varying vec3 vPosition;
         uniform sampler2D map;
 
-        float hash( ivec3 p )    // this hash is not production ready, please
-        {                        // replace this by something better
+        /* https://www.shadertoy.com/view/XsX3zB
+        *
+        * The MIT License
+        * Copyright © 2013 Nikita Miropolskiy
+        * 
+        * ( license has been changed from CCA-NC-SA 3.0 to MIT
+        *
+        *   but thanks for attributing your source code when deriving from this sample 
+        *   with a following link: https://www.shadertoy.com/view/XsX3zB )
+        *
+        * ~
+        * ~ if you're looking for procedural noise implementation examples you might 
+        * ~ also want to look at the following shaders:
+        * ~ 
+        * ~ Noise Lab shader by candycat: https://www.shadertoy.com/view/4sc3z2
+        * ~
+        * ~ Noise shaders by iq:
+        * ~     Value    Noise 2D, Derivatives: https://www.shadertoy.com/view/4dXBRH
+        * ~     Gradient Noise 2D, Derivatives: https://www.shadertoy.com/view/XdXBRH
+        * ~     Value    Noise 3D, Derivatives: https://www.shadertoy.com/view/XsXfRH
+        * ~     Gradient Noise 3D, Derivatives: https://www.shadertoy.com/view/4dffRH
+        * ~     Value    Noise 2D             : https://www.shadertoy.com/view/lsf3WH
+        * ~     Value    Noise 3D             : https://www.shadertoy.com/view/4sfGzS
+        * ~     Gradient Noise 2D             : https://www.shadertoy.com/view/XdXGW8
+        * ~     Gradient Noise 3D             : https://www.shadertoy.com/view/Xsl3Dl
+        * ~     Simplex  Noise 2D             : https://www.shadertoy.com/view/Msf3WH
+        * ~     Voronoise: https://www.shadertoy.com/view/Xd23Dh
+        * ~ 
+        *
+        */
 
-            // 3D -> 1D
-            int n = p.x*3 + p.y*113 + p.z*311;
-
-            // 1D hash by Hugo Elias
-            n = (n << 13) ^ n;
-            n = n * (n * n * 15731 + 789221) + 1376312589;
-            return float( n & ivec3(0x0fffffff))/float(0x0fffffff);
+        /* discontinuous pseudorandom uniformly distributed in [-0.5, +0.5]^3 */
+        vec3 random3(vec3 c) {
+            float j = 4096.0*sin(dot(c,vec3(17.0, 59.4, 15.0)));
+            vec3 r;
+            r.z = fract(512.0*j);
+            j *= .125;
+            r.x = fract(512.0*j);
+            j *= .125;
+            r.y = fract(512.0*j);
+            return r-0.5;
         }
 
-        float noise( in vec3 x )
-        {
-            ivec3 i = ivec3(floor(x));
-            vec3 f = fract(x);
-            f = f*f*(3.0-2.0*f);
+        /* skew constants for 3d simplex functions */
+        const float F3 =  0.3333333;
+        const float G3 =  0.1666667;
+
+        /* 3d simplex noise */
+        float simplex3d(vec3 p) {
+            /* 1. find current tetrahedron T and it's four vertices */
+            /* s, s+i1, s+i2, s+1.0 - absolute skewed (integer) coordinates of T vertices */
+            /* x, x1, x2, x3 - unskewed coordinates of p relative to each of T vertices*/
             
-            return mix(mix(mix( hash(i+ivec3(0,0,0)), 
-                                hash(i+ivec3(1,0,0)),f.x),
-                        mix( hash(i+ivec3(0,1,0)), 
-                                hash(i+ivec3(1,1,0)),f.x),f.y),
-                    mix(mix( hash(i+ivec3(0,0,1)), 
-                                hash(i+ivec3(1,0,1)),f.x),
-                        mix( hash(i+ivec3(0,1,1)), 
-                                hash(i+ivec3(1,1,1)),f.x),f.y),f.z);
+            /* calculate s and x */
+            vec3 s = floor(p + dot(p, vec3(F3)));
+            vec3 x = p - s + dot(s, vec3(G3));
+            
+            /* calculate i1 and i2 */
+            vec3 e = step(vec3(0.0), x - x.yzx);
+            vec3 i1 = e*(1.0 - e.zxy);
+            vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+                
+            /* x1, x2, x3 */
+            vec3 x1 = x - i1 + G3;
+            vec3 x2 = x - i2 + 2.0*G3;
+            vec3 x3 = x - 1.0 + 3.0*G3;
+            
+            /* 2. find four surflets and store them in d */
+            vec4 w, d;
+            
+            /* calculate surflet weights */
+            w.x = dot(x, x);
+            w.y = dot(x1, x1);
+            w.z = dot(x2, x2);
+            w.w = dot(x3, x3);
+            
+            /* w fades from 0.6 at the center of the surflet to 0.0 at the margin */
+            w = max(0.6 - w, 0.0);
+            
+            /* calculate surflet components */
+            d.x = dot(random3(s), x);
+            d.y = dot(random3(s + i1), x1);
+            d.z = dot(random3(s + i2), x2);
+            d.w = dot(random3(s + 1.0), x3);
+            
+            /* multiply d by w^4 */
+            w *= w;
+            w *= w;
+            d *= w;
+            
+            /* 3. return the sum of the four surflets */
+            return dot(d, vec4(52.0));
         }
 
         void main() {
 
             vec3 rgbColor = texture2D(map, vUv).rgb;
             
-            float noise = noise(vPosition * 5.0);
-            if (noise < 0.5) {
+            float noise = simplex3d(vPosition * 5.0);
+            if (noise < 0.0) {
 
                 noise = 1.0;
             }
